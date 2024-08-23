af17049bbe6cee6a219b21b63c9f314397bd874a
yosefbeder <dryosefbeder@gmail.com>
2024-08-23 06:34:20 +0300

    Add analytics and speed insights

--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Cairo } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "../globals.css";
 import Nav from "@/components/Nav";
@@ -21,6 +23,8 @@ export default function RootLayout({
       <body className={`${inter.variable} font-sans`}>
         <Nav />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
