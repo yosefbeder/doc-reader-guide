89b474fbee8e85147c2acf5cab21182ab1bd25ef
yosefbeder <dryosefbeder@gmail.com>
2024-09-10 09:24:39 +0300

    Make lecture cards the same size

---
+++ app/(content)/subjects/[subjectId]/components/Lecture.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+import { Lecture as LectureType } from "@/types";
+
+export default function Lecture({
+  lecture: { id, title, date },
+}: {
+  lecture: LectureType;
+}) {
+  return (
+    <Link href={`/lectures/${id}`} className="card min-h-36 group">
+      <h2
+        className={`line-clamp-2 group-hover:line-clamp-none max-[512px]:line-clamp-none`}
+      >
+        {title}
+      </h2>
+      <time dateTime={new Date(date).toISOString()}>
+        {new Date(date).toDateString()}
+      </time>
+    </Link>
+  );
+}
--- app/(content)/subjects/[subjectId]/components/Lectures.tsx
+++ app/(content)/subjects/[subjectId]/components/Lectures.tsx
@@ -1,19 +1,13 @@
-import Link from "next/link";
-
 import MasonryCardContainer from "@/components/MasonryCardContainer";
 import getLectures from "@/utils/getLectures";
+import Lecture from "./Lecture";
 
 export default async function Lectures({ subjectId }: { subjectId: number }) {
   const lectures = await getLectures(+subjectId);
   return (
     <MasonryCardContainer>
-      {lectures.map(({ id, title, date, createdAt }, index) => (
-        <Link key={index} href={`/lectures/${id}`} className="card">
-          <h2>{title}</h2>
-          <time dateTime={new Date(date || createdAt).toISOString()}>
-            {new Date(date || createdAt).toDateString()}
-          </time>
-        </Link>
+      {lectures.map((lecture, index) => (
+        <Lecture key={index} lecture={lecture} />
       ))}
     </MasonryCardContainer>
   );
