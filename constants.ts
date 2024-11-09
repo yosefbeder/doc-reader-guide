c75bf56625135e28b00db8792c0871cb8dd8447e
yosefbeder <dryosefbeder@gmail.com>
2024-11-09 12:28:42 +0200

    Change API url again

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://doc-reader-guide-server.com/api/v1";
+export const API_URL = "https://doc-reader-guide-server.com/api/v1";
 // export const API_URL = "http://localhost:8080/api/v1";
 export const VAPID_KEY =
   "BFF6xN5YLClIVapI5u7oGPtZkV6KHIWvc0AFGRWeWTuMk21gICu8iHzzNn-8BCrEFLfaoy1-1TuxgmVKmSPrVdY";
