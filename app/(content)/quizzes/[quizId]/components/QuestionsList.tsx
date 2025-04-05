5819b748f1610a31037f0d37bd50d47cf33d9cd9
Yosef Beder <dryosefbeder@gmail.com>
2025-04-05 11:56:10 +0200

    Update storing answers

--- app/(content)/quizzes/[quizId]/components/QuestionsList.tsx
+++ app/(content)/quizzes/[quizId]/components/QuestionsList.tsx
@@ -14,20 +14,21 @@ export default function QuestionsList({
   quizId: number;
   questions: Question[];
 }) {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [answers, setAnswers] = useState(questions.map(() => -1));
-  const [currentQuestionInput, setCurrentQuestionInput] = useState(1);
+  const [currentQuestion, setCurrentQuestion] = useState(questions[0].id);
+  const currentIndex = questions.findIndex(({ id }) => id === currentQuestion);
+  const [answers, setAnswers] = useState(new Map<number, number>());
   const [showingResults, setShowingResults] = useState(false);
-  const explanation = questions[currentQuestion].explanation;
+  const explanation = questions[currentIndex].explanation;
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
-    const quizJSON = localStorage.getItem(`quiz-${quizId}`);
+    const quizJSON = localStorage.getItem(`quiz-${quizId}-new`);
     if (quizJSON) {
       const quiz = JSON.parse(quizJSON);
-      setCurrentQuestion(quiz.currentQuestion);
+      if (questions.find(({ id }) => id === quiz.currentQuestion))
+        setCurrentQuestion(quiz.currentQuestion);
       setShowingResults(quiz.showingResults);
-      setAnswers(quiz.answers);
+      setAnswers(new Map(quiz.answers));
     }
     setIsLoaded(true);
   }, []);
@@ -35,21 +36,26 @@ export default function QuestionsList({
   useEffect(() => {
     if (isLoaded)
       localStorage.setItem(
-        `quiz-${quizId}`,
-        JSON.stringify({ currentQuestion, answers, showingResults })
+        `quiz-${quizId}-new`,
+        JSON.stringify({
+          currentQuestion,
+          answers: Array.from(answers),
+          showingResults,
+        })
       );
   }, [currentQuestion, answers, showingResults]);
 
-  useEffect(() => {
-    setCurrentQuestionInput(currentQuestion + 1);
-  }, [currentQuestion]);
-
   if (showingResults) {
-    const correct = answers.reduce(
-      (acc, answer, index) =>
-        answer === questions[index].correctOptionIndex ? acc + 1 : acc,
-      0
-    );
+    const correct = answers
+      .entries()
+      .reduce(
+        (acc, [questionId, optionIndex]) =>
+          optionIndex ===
+          questions.find(({ id }) => id === questionId)?.correctOptionIndex
+            ? acc + 1
+            : acc,
+        0
+      );
     return (
       <>
         <div className="flex gap-2 mb-4">
@@ -57,16 +63,16 @@ export default function QuestionsList({
           <ButtonIcon
             icon="arrow-path"
             onClick={() => {
-              setCurrentQuestion(0);
-              setAnswers(questions.map(() => -1));
-              setCurrentQuestionInput(1);
+              setCurrentQuestion(questions[0].id);
+              setAnswers(new Map());
               setShowingResults(false);
             }}
           />
         </div>
         <div className="max-w-lg" id="print-section">
           <h2 className="mb-4">
-            Result → {correct} / {questions.length}
+            Result → {correct} / {questions.length} (
+            {Math.round((correct / questions.length) * 10000) / 100}%)
           </h2>
           <h2 className="mb-4">Summary</h2>
           <p className="mb-4">
@@ -98,10 +104,10 @@ export default function QuestionsList({
                             optionIndex ===
                             questions[questionIndex].correctOptionIndex
                           )
-                            if (answers[questionIndex] === -1)
+                            if (!answers.has(question.id))
                               return "text-yellow-600";
                             else return "text-green-600";
-                          else if (answers[questionIndex] === optionIndex)
+                          else if (answers.get(question.id) === optionIndex)
                             return "text-red-600";
                         })()}`}
                       >
@@ -121,46 +127,49 @@ export default function QuestionsList({
   return (
     <div className="max-w-lg">
       <h2 className="mb-4">
-        Question {currentQuestion + 1} of {questions.length}
+        Question {currentIndex + 1} of {questions.length}
       </h2>
       <h3 className="p-4 rounded-xl bg-cyan-50 mb-4">
-        {questions[currentQuestion].text}
+        {questions[currentIndex].text}
       </h3>
-      {questions[currentQuestion].image ? (
+      {questions[currentIndex].image ? (
         <img
-          src={questions[currentQuestion].image}
+          src={questions[currentIndex].image}
           className="mb-4"
           alt="Question associated diagram"
         />
       ) : null}
       <ol className="list-[upper-alpha] list-inside flex flex-col gap-2 mb-4 px-2">
-        {questions[currentQuestion].options.map((option, index) => (
-          <button
-            key={index}
-            className={`block w-full text-left p-2 rounded-xl border transition-colors disabled:cursor-not-allowed ${(() => {
-              if (answers[currentQuestion] !== -1) {
-                if (questions[currentQuestion].correctOptionIndex === index)
-                  return "bg-green-100 hover:bg-green-200 border-green-600";
-                else if (answers[currentQuestion] === index)
-                  return "bg-red-100 hover:bg-red-200 border-red-600";
-              }
-              return "bg-slate-50 hover:bg-slate-100 border-slate-300";
-            })()}`}
-            disabled={answers[currentQuestion] !== -1}
-            onClick={() => {
-              if (answers[currentQuestion] !== -1) return;
-              setAnswers((prev) => [
-                ...prev.slice(0, currentQuestion),
-                index,
-                ...prev.slice(currentQuestion + 1),
-              ]);
-            }}
-          >
-            <li>{option}</li>
-          </button>
-        ))}
+        {questions[currentIndex].options.map((option, index) => {
+          const answer = answers.get(currentQuestion);
+          return (
+            <button
+              key={index}
+              className={`block w-full text-left p-2 rounded-xl border transition-colors disabled:cursor-not-allowed ${(() => {
+                if (answer !== undefined) {
+                  if (questions[currentIndex].correctOptionIndex === index)
+                    return "bg-green-100 hover:bg-green-200 border-green-600";
+                  else if (answer === index)
+                    return "bg-red-100 hover:bg-red-200 border-red-600";
+                }
+                return "bg-slate-50 hover:bg-slate-100 border-slate-300";
+              })()}`}
+              disabled={answer !== undefined}
+              onClick={() => {
+                if (answer) return;
+                setAnswers((prev) => {
+                  const newMap = new Map(prev);
+                  newMap.set(currentQuestion, index);
+                  return newMap;
+                });
+              }}
+            >
+              <li>{option}</li>
+            </button>
+          );
+        })}
       </ol>
-      {answers[currentQuestion] !== -1 && explanation ? (
+      {answers.has(currentQuestion) && explanation ? (
         <Message type="information" className="mb-4">
           {isValidURL(explanation) ? (
             <a href={explanation} target="_blank">
@@ -173,57 +182,33 @@ export default function QuestionsList({
       ) : null}
       <div className="flex justify-between mb-4">
         <Button
-          onClick={() => setCurrentQuestion((prev) => prev - 1)}
-          disabled={currentQuestion === 0}
+          onClick={() =>
+            setCurrentQuestion(() => questions[currentIndex - 1].id)
+          }
+          disabled={currentIndex === 0}
         >
           ← Back
         </Button>
-        <div className="flex gap-2 max-sm:hidden">
-          <input
-            type="number"
-            name="current-question"
-            id="current-question"
-            className="w-10"
-            value={currentQuestionInput}
-            onChange={(e) => setCurrentQuestionInput(e.target.valueAsNumber)}
-          />
-          <Button
-            onClick={() => setCurrentQuestion(currentQuestionInput - 1)}
-            disabled={
-              currentQuestionInput < 1 ||
-              currentQuestionInput > questions.length
-            }
-          >
-            Go
-          </Button>
-        </div>
         <Button
-          onClick={() => setCurrentQuestion((prev) => prev + 1)}
-          disabled={currentQuestion === questions.length - 1}
+          onClick={() =>
+            setCurrentQuestion(() => questions[currentIndex + 1].id)
+          }
+          disabled={currentIndex === questions.length - 1}
         >
-          {answers[currentQuestion] === -1 ? "Skip →" : "Continue →"}
+          {answers.has(currentQuestion) ? "Continue →" : "Skip →"}
         </Button>
       </div>
       <div className="flex justify-between mb-4">
-        <div className="max-sm:flex gap-2 hidden">
-          <input
-            type="number"
-            name="current-question"
-            id="current-question"
-            className="w-10"
-            value={currentQuestionInput}
-            onChange={(e) => setCurrentQuestionInput(e.target.valueAsNumber)}
-          />
-          <Button
-            onClick={() => setCurrentQuestion(currentQuestionInput - 1)}
-            disabled={
-              currentQuestionInput < 1 ||
-              currentQuestionInput > questions.length
-            }
-          >
-            Go
-          </Button>
-        </div>
+        <select
+          onChange={(e) => setCurrentQuestion(+e.target.value)}
+          value={currentQuestion}
+        >
+          {questions.map(({ id }, index) => (
+            <option key={id} value={id}>
+              Question {index + 1}
+            </option>
+          ))}
+        </select>
         <Button color="yellow" onClick={() => setShowingResults(true)}>
           End Quiz
         </Button>
