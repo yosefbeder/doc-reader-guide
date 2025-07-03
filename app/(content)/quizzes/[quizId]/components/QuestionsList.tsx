3175ffe438fbb7f520f2814b4969846f25eef32a
Yosef Beder <dryosefbeder@gmail.com>
2025-07-03 08:10:22 +0300

    Fix printing on safari mobile

--- app/(content)/quizzes/[quizId]/components/QuestionsList.tsx
+++ app/(content)/quizzes/[quizId]/components/QuestionsList.tsx
@@ -45,6 +45,7 @@ export default function QuestionsList({
           "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
       },
     ],
+    preserveAfterPrint: true,
   });
 
   useHotkeys("left", backQuestion, [currentIndex, questions]);
