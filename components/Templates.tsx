0ca5014860ef205ddc0cbc031f44d54b7056760a
Yosef Beder <dryosefbeder@gmail.com>
2025-07-11 19:50:24 +0300

    Fix templates design

--- components/Templates.tsx
+++ components/Templates.tsx
@@ -57,7 +57,7 @@ export default function Templates() {
         required
         className="max-w-lg"
       />
-      <div className="flex gap-2 max-sm:flex-col">
+      <div className="flex flex-col items-start gap-2">
         <a
           href={model + MCQ_FILE_NO_ANSWERS}
           target="_blank"
