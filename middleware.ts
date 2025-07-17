f2f30dfe999fd49851576a2132edcb5b1d9c5dc4
Yosef Beder <dryosefbeder@gmail.com>
2025-07-17 07:42:36 +0300

    Make middleware not run for .well-known

--- middleware.ts
+++ middleware.ts
@@ -29,6 +29,6 @@ export async function middleware(req: NextRequest): Promise<NextResponse> {
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|images|favicon.ico|sitemap.xml|robots.txt).*)",
+    "/((?!api|_next/static|_next/image|.well-known|images|favicon.ico|sitemap.xml|robots.txt).*)",
   ],
 };
