d35816a35c4f2e0e98163e4364684822e8a81e59
Yosef Beder <dryosefbeder@gmail.com>
2025-08-15 19:13:42 +0300

    Fix getFaculties

--- utils/getFaculties.ts
+++ utils/getFaculties.ts
@@ -1,9 +1,12 @@
 import { Faculty } from "@/types";
 
 export default async function getFaculties(): Promise<Faculty[]> {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/faculties`, {
-    credentials: "include",
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/faculties?include=years`,
+    {
+      credentials: "include",
+    }
+  );
   const json = await res.json();
   if (!res.ok) throw new Error(json.message);
   return json.data.faculties;
