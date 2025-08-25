7a6b53a5527222bc303ea057c04ad791507df94c
Yosef Beder <dryosefbeder@gmail.com>
2025-08-25 10:48:39 +0300

    Fix build error

--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -2,28 +2,10 @@ import { Metadata } from "next";
 
 import buildCanonical from "@/utils/buildCanonical";
 
-export async function generateMetadata({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined };
-}): Promise<Metadata> {
+export async function generateMetadata(): Promise<Metadata> {
   const title = "Log in | DocReader Guide";
   const description =
     "Access your personalized home page on Doc Reader Guide. Log in to explore lectures, notes, and quizzes tailored to your curriculum.";
-
-  const hasRedirect = !!searchParams?.redirect;
-
-  if (hasRedirect) {
-    return {
-      title,
-      description,
-      robots: {
-        index: false,
-        follow: true,
-      },
-      ...buildCanonical("/login"),
-    };
-  }
 
   return {
     title,
