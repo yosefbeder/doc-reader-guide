0674b14d90f98b1353d99f9c74e5958da7bffd3e
Yosef Beder <dryosefbeder@gmail.com>
2025-09-14 07:57:45 +0300

    Improve LinkCard responsiveness

--- app/(content)/lectures/[lectureId]/components/LinkCard.tsx
+++ app/(content)/lectures/[lectureId]/components/LinkCard.tsx
@@ -16,64 +16,57 @@ export default function LinkCard({
   onUpdate?: () => void;
 }) {
   return (
-    <div className="flex items-center superficial gap-2 p-2 rounded-xl bg-white">
-      {urls.length === 1 ? (
-        <a
-          className="grow flex items-center gap-2 reset-link"
-          target="_blank"
-          href={urls[0]}
-          onClick={() =>
-            logEvent(Resource.LINK, id, Action.NAVIGATE, { url: urls[0] })
+    <div className="flex flex-col superficial rounded-xl bg-white">
+      <div
+        onClick={() => {
+          if (urls.length === 1) {
+            window.open(urls[0], "_blank");
+            logEvent(Resource.LINK, id, Action.NAVIGATE, { url: urls[0] });
           }
-        >
-          {typeIcons[type]}
+        }}
+        className={`w-full flex items-center gap-2 p-2 ${
+          urls.length === 1 ? "cursor-pointer" : ""
+        }`}
+      >
+        <div className="w-max">{typeIcons[type]}</div>
+        <div className="grow">
           {subTitle?.trim() ? (
-            <div>
+            <>
               <div>{title}</div>
               <div className="text-sm text-slate-500">{subTitle}</div>
-            </div>
+            </>
           ) : (
             title
           )}
-        </a>
-      ) : (
-        <>
-          <div className="grow flex items-center gap-2">
-            {typeIcons[type]}
-            {subTitle?.trim() ? (
-              <div>
-                <div>{title}</div>
-                <div className="text-sm text-slate-500">{subTitle}</div>
-              </div>
-            ) : (
-              title
-            )}
-          </div>
-          <ul className="flex items-center gap-2">
-            {urls.map((url, index) => (
-              <li key={index}>
-                <a
-                  className="link"
-                  href={url}
-                  target="_blank"
-                  onClick={() =>
-                    logEvent(Resource.LINK, id, Action.NAVIGATE, { url })
-                  }
-                >
-                  {index + 1}
-                  <sup>{getPrefix(index + 1)}</sup>
-                </a>
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
-
-      {updateable && (
-        <ButtonIcon
-          icon="pencil-square"
-          onClick={() => onUpdate && onUpdate()}
-        />
+        </div>
+        {updateable && (
+          <ButtonIcon
+            icon="pencil-square"
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              onUpdate && onUpdate();
+            }}
+          />
+        )}
+      </div>
+      {urls.length > 1 && (
+        <ul className="w-full rounded-b-xl flex items-center gap-4 px-4 py-1 bg-cyan-600 text-white underline">
+          {urls.map((url, index) => (
+            <li key={index}>
+              <a
+                href={url}
+                target="_blank"
+                onClick={() =>
+                  logEvent(Resource.LINK, id, Action.NAVIGATE, { url })
+                }
+              >
+                {index + 1}
+                <sup>{getPrefix(index + 1)}</sup>
+              </a>
+            </li>
+          ))}
+        </ul>
       )}
     </div>
   );
