9dfbdae821d5149fd2904a6835a734c8c3f280f3
Yosef Beder <dryosefbeder@gmail.com>
2025-12-10 19:58:31 +0200

    Add omar account in partner with us button

--- app/(content)/(landing-page)/components/UniversityRequest.tsx
+++ app/(content)/(landing-page)/components/UniversityRequest.tsx
@@ -5,24 +5,21 @@ export default function UniversityRequest({ lang }: { lang: "en" | "ar" }) {
 
   return (
     <section
-      className={`py-8 bg-slate-50 dark:bg-slate-900/50 border-t border-slate-200 dark:border-slate-800 ${isAr ? "font-arabic" : ""
-        }`}
+      className={`py-8 bg-slate-50 dark:bg-slate-900/50 border-t border-slate-200 dark:border-slate-800 ${
+        isAr ? "font-arabic" : ""
+      }`}
     >
       <div className="main flex flex-col gap-8 items-center text-center">
         <h2>
-          {isAr
-            ? "كُن المبادر في كليتك"
-            : "Lead the Change at Your Faculty"}
+          {isAr ? "كُن المبادر في كليتك" : "Lead the Change at Your Faculty"}
         </h2>
         <p className="max-w-2xl">
           {isAr
             ? "هل أنت من ممثلي الدفعة أو اتحاد الطلاب؟ تواصل معنا الآن لإنشاء مساحة مخصصة وتنظيم المحتوى العلمي لدفعتك بالكامل على التطبيق."
             : "Are you a student union member or batch leader? Partner with us to create a dedicated, organized space for your entire faculty on DocReader."}
         </p>
-        <a href="https://t.me/DocReader_Guide_app" target="_blank">
-          <Button cta>
-            {isAr ? "تعاون معنا" : "Partner with Us"}
-          </Button>
+        <a href="https://t.me/Omar_AbdelAleem" target="_blank">
+          <Button cta>{isAr ? "تعاون معنا" : "Partner with Us"}</Button>
         </a>
       </div>
     </section>
