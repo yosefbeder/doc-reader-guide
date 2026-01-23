fc81492c6c0204095387bbf3edc293354a8b5ee2
Yosef Beder <dryosefbeder@gmail.com>
2026-01-23 22:23:50 +0200

    Add warning for in-app authentication

--- app/(auth)/login/components/LoginContent.tsx
+++ app/(auth)/login/components/LoginContent.tsx
@@ -7,6 +7,26 @@ import Logo from "@/public/logo.png";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Suspense } from "react";
 import GoogleButton from "./GoogleButton";
+import Message from "@/components/Message";
+
+function isInAppBrowser() {
+  const ua = navigator.userAgent || navigator.vendor;
+
+  return (
+    /Telegram/i.test(ua) ||
+    /FBAN|FBAV/i.test(ua) ||
+    /Instagram/i.test(ua) ||
+    /Twitter/i.test(ua) ||
+    /LinkedIn/i.test(ua) ||
+    /Snapchat/i.test(ua) ||
+    /TikTok/i.test(ua) ||
+    /Line/i.test(ua) ||
+    /WeChat/i.test(ua) ||
+    /WhatsApp/i.test(ua) ||
+    /GSA/i.test(ua) ||
+    /\bwv\b/.test(ua)
+  );
+}
 
 export default function LoginContent() {
   const [scriptLoaded, setScriptLoaded] = useState(false);
@@ -24,7 +44,12 @@ export default function LoginContent() {
           <span className="text-4xl font-extrabold text-cyan-700 dark:text-cyan-500">
             DocReader Guide
           </span>
-
+          {isInAppBrowser() && (
+            <Message type="warning">
+              Please tap the three dots (⋮) and select &quot;Open in
+              Chrome/Safari&quot;. Some in-app browsers block Google.
+            </Message>
+          )}
           <GoogleButton scriptLoaded={scriptLoaded} scriptError={scriptError} />
           <Link href="/" className="link">
             ← Back to Home
