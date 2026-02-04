e2ed653ecfd98ce49fd46a64ab554d3282ea7fec
Yosef Beder <dryosefbeder@gmail.com>
2026-02-04 19:07:55 +0200

    Fix faculties limit

--- utils/getFaculties.ts
+++ utils/getFaculties.ts
@@ -2,7 +2,7 @@ import { Faculty } from "@/types";
 
 export default async function getFaculties(): Promise<Faculty[]> {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/faculties?include=years&sort=createdAt`,
+    `${process.env.NEXT_PUBLIC_API_URL}/faculties?include=years&sort=createdAt&size=100`,
     {
       credentials: "include",
     }
